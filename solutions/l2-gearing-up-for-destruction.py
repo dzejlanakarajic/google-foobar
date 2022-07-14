5dcf880738145aff5aba580140d52e15e61025dc
dzejlanakarajic <dzejlana.karajic@gmail.com>
2022-07-14 09:35:52 +0200

    Update l2-gearing-up-for-destruction.py

--- solutions/l2-gearing-up-for-destruction.py
+++ solutions/l2-gearing-up-for-destruction.py
@@ -1,36 +1,31 @@
-from fractions import Fraction
-
-
 def solution(pegs):
     dist = []
-
     for p in range(len(pegs) - 1):
         dist += [pegs[p + 1] - pegs[p]]
-    result = []
-    if len(dist) % 2 == 1:
-        r2 = (Fraction(dist[0], 3))
-        if r2 < 0:
-            return [-1,-1]
-        for num in dist:
-            result.append(num-r2)
-        result.insert(1, r2)
 
-    elif len(dist) % 2 == 0:
-        temp = dist[:]
-        while len(temp) > 2:
-            temp[-1] = temp[-2] - temp[-1]
-            temp.pop(-2)
-        r2 = (temp[1] * 2) - temp[0]
+    gear_radius = [dist[0] / dist[0]]
 
-        result = [dist[0] - r2, r2]
+    start = 0
+    while len(gear_radius) <= len(dist):
+        gear_radius.append(dist[start] - gear_radius[start])
+        start += 1
 
-        while len(result) != len(dist) + 1:
-            for num in range(len(dist) - 1):
-                result.append(dist[num + 1] - result[num + 1])
-        negatives = sum(n < 0 for n in result)
-        if negatives > 0:
-            return [-1, -1]
-    if result[0].denominator == result[-1].denominator and result[0].numerator == result[-1].numerator * 2:
-        return [result[0].numerator, result[0].denominator]
+    while gear_radius[1] != 1:
+        for num in range(len(gear_radius)):
+            if num % 2 == 0:
+                gear_radius[num] = gear_radius[num] + 1
+            else:
+                gear_radius[num] = gear_radius[num] - 1
 
-    return [-1,-1]
+        if any(d <= 0 for d in gear_radius):
+            continue
+
+        if gear_radius[0] == 2 * gear_radius[-1]:
+            return [gear_radius[0], 1]
+
+        elif gear_radius[0] + 1 == 2 * gear_radius[-1]:
+            return [(gear_radius[0] * 3) + 1, 3]
+        elif gear_radius[0] + 2 == 2 * gear_radius[-1]:
+            return [(gear_radius[0] * 3) + 2, 3]
+
+    return [-1, -1]
